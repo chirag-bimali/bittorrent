79f595ae16f076f9003248ae20309c58b2c58a08
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-09-02 17:30:45 +0545

    Decode Dictonary

--- app/main.ts
+++ app/main.ts
@@ -1,65 +1,80 @@
 // Examples:
+
 // - decodeBencode("5:hello") -> "hello"
+
 // - decodeBencode("10:hello12345") -> "hello12345"
 
+type Dictionary = { [key: string]: any };
+
 function decodeBencodeString(bencodedValue: string): [string, number] {
-  // Check for strings
-  // const stringLength = parseInt(bencodedValue.substring(0, firstColonIndex));
-
-  // if (!isNaN(stringLength)) {
-  //   if (firstColonIndex === -1) {
-  //     throw new Error("Invalid encoded value");
-  //   }
-  //   return JSON.stringify(bencodedValue.substring(firstColonIndex + 1));
-  // }
-
   if (!isNaN(parseInt(bencodedValue[0]))) {
     let digitBuffer = "";
+
     let indexOfColon = 0;
+
     for (indexOfColon; bencodedValue[indexOfColon] != ":"; indexOfColon++) {
       digitBuffer += bencodedValue[indexOfColon];
     }
+
     const digits = parseInt(digitBuffer);
+
     const value = bencodedValue.slice(
       indexOfColon + 1,
       indexOfColon + 1 + digits
     );
-    return [JSON.stringify(value), digitBuffer.length + 1 + value.length];
+
+    // return [JSON.stringify(value), digitBuffer.length + 1 + value.length];
+    return [value, digitBuffer.length + 1 + value.length];
   }
 
-  throw new Error("Invalid encoded value");
+  throw new Error(`Invalid string bencoded value ${bencodedValue}`);
 }
 
 function decodeBencodeInteger(bencodedValue: string): [number, number] {
   // Check integer
+
   if (bencodedValue.startsWith("i")) {
     const valueString = parseInt(
       bencodedValue.slice(1, bencodedValue.indexOf("e"))
     );
+
     if (isNaN(valueString))
       throw new Error(`Invalid encoded value type ${valueString}`);
+
     return [valueString, 1 + valueString.toString().length + 1];
   }
-  throw new Error("Invalid encoded value");
+
+  throw new Error(`Invalid integer bencoded value ${bencodedValue}`);
 }
 
 function decodeBencodeList(bencodedValue: string): [Array<any>, number] {
   const arrayBuffer: any[] = [];
+
   let decodedLength = 1;
+
   //  Check list
+
   if (bencodedValue.startsWith("l")) {
     //  Check values
+
     // First extract values as a whole string
+
     let valueString = bencodedValue.slice(1);
-    console.log(valueString);
+
     do {
       if (valueString.startsWith("i")) {
         const [value, decodedNumberLength] = decodeBencodeInteger(valueString);
+
         arrayBuffer.push(value);
+
         valueString = valueString.substring(decodedNumberLength);
+        console.log(valueString);
+
         decodedLength += decodedNumberLength;
+
         continue;
       }
+
       if (!isNaN(parseInt(valueString[0]))) {
         const [value, decodedStringLength] = decodeBencodeString(valueString);
         arrayBuffer.push(value);
@@ -67,6 +82,7 @@ function decodeBencodeList(bencodedValue: string): [Array<any>, number] {
         decodedLength += decodedStringLength;
         continue;
       }
+
       if (valueString.startsWith("l")) {
         const [array, decodedSubArrayLength] = decodeBencodeList(valueString);
         arrayBuffer.push(array);
@@ -74,43 +90,124 @@ function decodeBencodeList(bencodedValue: string): [Array<any>, number] {
         valueString = valueString.substring(decodedSubArrayLength);
         continue;
       }
+      if (valueString.startsWith("d")) {
+        const [dictonary, decodeValueLegth] =
+          decodeBencodeDictonary(valueString);
+        arrayBuffer.push(dictonary);
+        decodedLength += decodeValueLegth;
+        valueString = valueString.substring(decodeValueLegth);
+        continue;
+      }
+
       if (valueString.startsWith("e")) {
         valueString = valueString.substring(1);
         decodedLength++;
         return [arrayBuffer, decodedLength];
       }
+
       break;
     } while (true);
 
     return [arrayBuffer, decodedLength];
   }
 
+  throw new Error(`Invalid list bencoded value ${bencodedValue}`);
+}
+
+function decodeBencodeDictonary(bencodedValue: string): [Dictionary, number] {
+  // d3:cow3:moo4:spam4:eggse
+
+  console.log(bencodedValue)
+  const dictonaryBuffer: Dictionary = {};
+  let valueString = bencodedValue.slice(1);
+  let decodedLength = 1;
+  if (bencodedValue.startsWith("d")) {
+    do {
+      if (valueString.startsWith("e")) {
+        valueString = valueString.substring(1);
+        decodedLength++;
+        return [dictonaryBuffer, decodedLength];
+      }
+
+      const [key, decodedKeyLength] = decodeBencodeString(valueString);
+      valueString = valueString.substring(decodedKeyLength);
+      decodedLength += decodedKeyLength;
+
+      if (valueString.startsWith("i")) {
+        const [integerValue, decodedValueLength] =
+          decodeBencodeInteger(valueString);
+        decodedLength += decodedValueLength;
+        valueString = valueString.substring(decodedValueLength);
+        dictonaryBuffer[key] = integerValue;
+        continue;
+      }
+
+      if (valueString.startsWith("l")) {
+        const [listValue, decodedValueLength] = decodeBencodeList(valueString);
+        decodedLength += decodedValueLength;
+        valueString = valueString.substring(decodedValueLength);
+        dictonaryBuffer[key] = listValue;
+        continue;
+      }
+
+      if (valueString.startsWith("d")) {
+        const [dictonary, decodeValueLegth] =
+          decodeBencodeDictonary(valueString);
+        decodedLength += decodeValueLegth;
+        valueString = valueString.substring(decodeValueLegth);
+        dictonaryBuffer[key] = dictonary;
+        continue;
+      }
+
+      if (!isNaN(parseInt(valueString[0]))) {
+        const [value, decodedValueLength] = decodeBencodeString(valueString);
+        dictonaryBuffer[key] = value;
+        valueString = valueString.substring(decodedValueLength);
+        decodedLength += decodedValueLength;
+        continue;
+      }
+
+      throw new Error(`Invalid dictonary bencoded value ${bencodedValue}`);
+    } while (true);
+  }
+
   throw new Error("Invalid encoded value");
 }
 
-function decodeBencode(bencodedValue: string): string | number | Array<any> {
+function decodeBencode(
+  bencodedValue: string
+): string | number | Array<any> | Dictionary {
   let value;
+
   let decodedSize;
 
   if (bencodedValue.startsWith("l"))
     [value, decodedSize] = decodeBencodeList(bencodedValue);
   else if (bencodedValue.startsWith("i"))
     [value, decodedSize] = decodeBencodeInteger(bencodedValue);
+  else if (bencodedValue.startsWith("d"))
+    [value, decodedSize] = decodeBencodeDictonary(bencodedValue);
   else [value, decodedSize] = decodeBencodeString(bencodedValue);
+
   return value;
 }
 
 const args = process.argv;
+
 const bencodedValue = args[3];
 
 if (args[2] === "decode") {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
+
   console.error("Logs from your program will appear here!");
 
   // Uncomment this block to pass the first stage
+
   try {
     const decoded = decodeBencode(bencodedValue);
+
     console.log(decoded);
+
     // console.log(JSON.stringify(decoded));
   } catch (error) {
     console.error(error.message);
