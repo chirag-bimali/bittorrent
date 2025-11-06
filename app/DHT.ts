a6610f0c5d1cb25044c770b9d038d9c290c01286
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-11-06 21:23:26 +0545

    Fix Udp get response issue

--- app/DHT.ts
+++ app/DHT.ts
@@ -2,7 +2,7 @@ import { exit } from "process";
 import dgram from "dgram";
 import BencodeEncoder from "./bencodeEncoder";
 import BencodeDecoder from "./bencodeDecoder";
-import crypto from "crypto"
+import crypto from "crypto";
 
 export interface NodeInfo {
   id: Buffer;
@@ -12,8 +12,8 @@ export interface NodeInfo {
 // bucket operation
 // insert node ✓
 // read node ✓
-// update node 
-// delete node ✓ 
+// update node
+// delete node ✓
 // check bucket compatibility
 // has space operation
 
@@ -57,12 +57,12 @@ export class Bucket {
     return [newBuckets[0], newBuckets[1]];
   }
   find(callBackfn: (node: NodeInfo) => boolean): NodeInfo | null {
-    const node = this.nodes.find(callBackfn)
+    const node = this.nodes.find(callBackfn);
     return node ? node : null;
   }
   delete(callBackfn: (node: NodeInfo) => boolean): NodeInfo[] | null {
     const index = this.nodes.findIndex(callBackfn);
-    if(index < 0) return null;
+    if (index < 0) return null;
     return this.nodes.splice(index, 1);
   }
   static bufferToBigint(buffer: Buffer): bigint {
@@ -118,17 +118,17 @@ export class RoutingTable {
       );
     this.buckets[index].insert(node);
   }
-  find(callbackFn:(node: NodeInfo) => boolean): NodeInfo | null {
-    for(const bucket of this.buckets) {
-      const node = bucket.find(callbackFn)
-      if(node) return node
+  find(callbackFn: (node: NodeInfo) => boolean): NodeInfo | null {
+    for (const bucket of this.buckets) {
+      const node = bucket.find(callbackFn);
+      if (node) return node;
     }
     return null;
   }
   delete(callBackfn: (node: NodeInfo) => boolean): NodeInfo[] | null {
-    for(const bucket of this.buckets) {
-      const node = bucket.delete(callBackfn)
-      if(node !== null) return node;
+    for (const bucket of this.buckets) {
+      const node = bucket.delete(callBackfn);
+      if (node !== null) return node;
     }
     return null;
   }
@@ -136,15 +136,13 @@ export class RoutingTable {
 export default class DHT {
   public routingTable: RoutingTable;
   public maxIdSpace: bigint;
-  public infoHashNode: Map<Buffer, NodeInfo[]> = new Map<Buffer, NodeInfo[]>
+  public infoHashNode: Map<Buffer, NodeInfo[]> = new Map<Buffer, NodeInfo[]>();
   public PORT: number = 6881;
-  public HOST: string = '0.0.0.0';
-  public BOOTSTRAP_NODE = 'router.bittorrent.com';
+  public HOST: string = "0.0.0.0";
+  public BOOTSTRAP_NODE = "router.bittorrent.com";
   public BOOTSTRAP_NODE_PORT = 6881;
   public ID: Buffer;
-  private client =  dgram.createSocket("udp4");
-  private server =  dgram.createSocket("udp4");
-  
+  private client = dgram.createSocket("udp4");
 
   constructor(maxIdSpace: bigint, clientId: Buffer) {
     this.routingTable = new RoutingTable(0n, maxIdSpace, clientId);
@@ -153,63 +151,75 @@ export default class DHT {
   }
   // ping, find_node, get_peers, and announce_peer.
   /*
-  * "t": "transaction-id"
-  * "y": "q" | "r" | "e"
-  * "y": "q" | "r" | "e"
-  * "v": "versioning stuff" // not necessary
-  */
+   * "t": "transaction-id"
+   * "y": "q" | "r" | "e"
+   * "y": "q" | "r" | "e"
+   * "v": "versioning stuff" // not necessary
+   */
   /*
-  * Initialize DHT by calling to Bootstrap node
-  * Join the network first
-  */
+   * Initialize DHT by calling to Bootstrap node
+   * Join the network first
+   */
   initialie() {
-    this.server.on("message", (msg: Buffer, rinfo) => {
+    this.client.on("message", (msg: Buffer, rinfo) => {
       console.log("Data received");
-      console.log(msg)
-      console.log(rinfo)
-    })
-    this.server.on("listening", () => {
-      console.log(`Listening on ${this.HOST}:${this.PORT}`)
+      console.log(msg);
+      console.log(rinfo);
+    });
+    this.client.on("listening", () => {
+      console.log(`Listening on ${this.HOST}:${this.PORT}`);
       this.ping();
+    });
+    this.client.on("error", (err) => {
+      console.error("DHT socket error:", err)
     })
-    this.server.bind(this.PORT, this.HOST);
+    this.client.bind(this.PORT, this.HOST);
   }
 
   /*
-  * Ping the node
-  */
+   * Ping the node
+   */
   ping() {
-    const tId = crypto.createHash("sha1").update(Math.random().toString(), "binary").digest().subarray(0, 2); 
-    console.log(`Length of transaction id: ${tId.length}`)
-    const msg = {
-      "t" : tId,  
-      "y" : "q",  
-      "q" : "ping",  
-      "a" : { "id" : this.ID}
-    }
-    const encoded = BencodeEncoder.bencodeDictonary(msg);
-  
-    
-    this.client.send(encoded, 0, encoded.length, this.BOOTSTRAP_NODE_PORT, this.BOOTSTRAP_NODE, (err) => {
-      console.log(`Datagram sent`);
-      console.log(`Error:`);
-      console.log(err);
-    })
+    const tId = crypto
+      .createHash("sha1")
+      .update(Math.random().toString(), "utf-8")
+      .digest()
+      .subarray(0, 2);
+    const tIdString = tId.toString("binary");
+    const clientIdString = this.ID.toString("binary");
 
+    const msg = {
+      t: tIdString,
+      y: "q",
+      q: "ping",
+      a: { id: clientIdString },
+    };
+    const encoded = BencodeEncoder.bencodeDictonary(msg);
+    const encodedBuf = Buffer.from(String(encoded), "binary");
+    this.client.send(
+      encodedBuf,
+      this.BOOTSTRAP_NODE_PORT,
+      this.BOOTSTRAP_NODE,
+      (err) => {
+        console.log(`Datagram sent`);
+        console.log(`Error:`);
+        console.log(err);
+      }
+    );
   }
 
   /*
-  * Find query to the node
-  */
+   * Find query to the node
+   */
   findNnode() {}
 
   /*
-  * Get peers query to the node
-  */
+   * Get peers query to the node
+   */
   getPeers() {}
 
   /*
-  * Announce peers query to the node
-  */
+   * Announce peers query to the node
+   */
   announcePeer() {}
 }
